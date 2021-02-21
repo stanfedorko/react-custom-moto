5bd5be15953f484fa087930b309b8635d074ffcd
Stas Fedorko <stasfedorko@MacBook-Pro-Stas.local>
2021-02-21 16:23:24 +0100

    Updates

---
+++ precache-manifest.a2823ea5837f2794cb4d9ad755bcda5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b584079cd066ddfd640be6885e2a36c",
+    "url": "/react-custom-moto/index.html"
+  },
+  {
+    "revision": "8ad4b82a052af77715a6",
+    "url": "/react-custom-moto/static/js/2.96d8bd63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-custom-moto/static/js/2.96d8bd63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f5cad7ef9b16583fd6",
+    "url": "/react-custom-moto/static/js/main.8eaf8a26.chunk.js"
+  },
+  {
+    "revision": "3ee44f660f60ed9a0fea",
+    "url": "/react-custom-moto/static/js/runtime-main.226fada5.js"
+  }
+]);
